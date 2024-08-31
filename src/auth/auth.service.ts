65f1f803488b3246b4d831ab1ff151a496698366
Ovie Revaldi <ovierevaldi@gmail.com>
2024-08-31 19:42:41 +0700

    Add JWT Login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+
+const fakeUser = [
+    {
+        id: 1,
+        username: 'ovie',
+        password: 'abc123'
+    }
+]
+
+@Injectable()
+export class AuthService {
+    constructor(private jwtService: JwtService){
+
+    }
+    
+    validateUser({username, password}: AuthPayloadDto){
+        const findUser =  fakeUser.find((user) => user.username == username);
+
+        if(!findUser) return null;
+        if(password === findUser.password){
+            const {password, ...user} = findUser;
+            return this.jwtService.sign(user);
+        }
+    }
+}
